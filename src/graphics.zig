35adfecc61ab2e25d02b35a1947592d0043d7139
gabriel miranda <gabrielmfern@outlook.com>
2025-12-11 09:01:03 -0300

    wip: graphics and presentation queues, creation of logical device

--- src/graphics.zig
+++ src/graphics.zig
@@ -37,6 +37,9 @@ const VulkanError = error{
     OutOfDeviceMemory,
     OutOfHostMemory,
     ValidationFailed,
+    FeatureNotPresent,
+    DeviceLost,
+    TooManyObjects,
     Unknown,
 };
 
@@ -55,6 +58,12 @@ fn ensureNoError(result: c.VkResult) !void {
         return error.OutOfHostMemory;
     } else if (result == c.VK_ERROR_VALIDATION_FAILED) {
         return error.ValidationFailed;
+    } else if (result == c.VK_ERROR_FEATURE_NOT_PRESENT) {
+        return error.FeatureNotPresent;
+    } else if (result == c.VK_ERROR_DEVICE_LOST) {
+        return error.DeviceLost;
+    } else if (result == c.VK_ERROR_TOO_MANY_OBJECTS) {
+        return error.TooManyObjects;
     } else if (result == c.VK_ERROR_UNKNOWN) {
         return error.Unknown;
     }
@@ -142,23 +151,22 @@ fn validateExtensions(extensions: []const [*c]const u8, allocator: std.mem.Alloc
 
 vulkanInstance: c.VkInstance,
 vulkanDebugMessenger: c.VkDebugUtilsMessengerEXT,
-physicalDevice: c.VkPhysicalDevice,
 
 pub fn init(name: [*c]const u8, allocator: std.mem.Allocator) !@This() {
-    const extensions: []const [*c]const u8 = &.{
+    const instanceExtensions: []const [*c]const u8 = &.{
         c.VK_KHR_SURFACE_EXTENSION_NAME,
         "VK_KHR_wayland_surface",
         c.VK_KHR_PORTABILITY_ENUMERATION_EXTENSION_NAME,
         c.VK_EXT_DEBUG_UTILS_EXTENSION_NAME,
     };
 
-    const layers: []const [*c]const u8 = &.{
+    const instanceLayers: []const [*c]const u8 = &.{
         "VK_LAYER_KHRONOS_validation",
     };
 
-    try validateLayers(layers, allocator);
+    try validateLayers(instanceLayers, allocator);
 
-    try validateExtensions(extensions, allocator);
+    try validateExtensions(instanceExtensions, allocator);
 
     var vulkanInstance: c.VkInstance = undefined;
     try ensureNoError(c.vkCreateInstance(
@@ -175,10 +183,10 @@ pub fn init(name: [*c]const u8, allocator: std.mem.Allocator) !@This() {
                 .engineVersion = c.VK_MAKE_VERSION(1, 0, 0),
                 .apiVersion = c.VK_API_VERSION_1_0,
             },
-            .enabledLayerCount = layers.len,
-            .ppEnabledLayerNames = layers.ptr,
-            .enabledExtensionCount = extensions.len,
-            .ppEnabledExtensionNames = extensions.ptr,
+            .enabledLayerCount = instanceLayers.len,
+            .ppEnabledLayerNames = instanceLayers.ptr,
+            .enabledExtensionCount = instanceExtensions.len,
+            .ppEnabledExtensionNames = instanceExtensions.ptr,
         },
         // TODO: define a proper allocator here using an allocator from the outside
         null,
@@ -239,21 +247,44 @@ pub fn init(name: [*c]const u8, allocator: std.mem.Allocator) !@This() {
     );
     std.debug.assert(vulkanDebugMessenger != null);
 
+    return .{
+        .vulkanInstance = vulkanInstance,
+        .vulkanDebugMessenger = vulkanDebugMessenger,
+    };
+}
+
+const Device = struct {
+    physicalDevice: c.VkPhysicalDevice,
+
+    logicalDevice: c.VkDevice,
+    graphicsQueue: c.VkQueue,
+    presentationQueue: c.VkQueue,
+};
+
+fn createDevice(self: @This(), surface: c.VkSurfaceKHR, allocator: std.mem.Allocator) !Device {
     var physicalDevicesLen: u32 = undefined;
     try ensureNoError(c.vkEnumeratePhysicalDevices(
-        vulkanInstance,
+        self.vulkanInstance,
         &physicalDevicesLen,
         null,
     ));
-    const physicalDevices = try allocator.alloc(c.VkPhysicalDevice, @intCast(physicalDevicesLen));
+    const physicalDevices = try allocator.alloc(
+        c.VkPhysicalDevice,
+        @intCast(physicalDevicesLen),
+    );
     errdefer allocator.free(physicalDevices);
     try ensureNoError(c.vkEnumeratePhysicalDevices(
-        vulkanInstance,
+        self.vulkanInstance,
         &physicalDevicesLen,
         physicalDevices.ptr,
     ));
 
-    var preferred: ?struct { device: c.VkPhysicalDevice, score: u32 } = null;
+    var preferred: ?struct {
+        device: c.VkPhysicalDevice,
+        features: c.VkPhysicalDeviceFeatures,
+        queues: QueueIndices,
+        score: u32,
+    } = null;
     for (physicalDevices) |device| {
         var deviceProperties: c.VkPhysicalDeviceProperties = undefined;
         c.vkGetPhysicalDeviceProperties(device, &deviceProperties);
@@ -264,6 +295,11 @@ pub fn init(name: [*c]const u8, allocator: std.mem.Allocator) !@This() {
             continue;
         }
 
+        const queueIndices = try getQueueIndices(device, surface, allocator);
+        if (queueIndices == null) {
+            continue;
+        }
+
         var score: u32 = deviceProperties.limits.maxImageDimension2D;
         if (deviceProperties.deviceType != c.VK_PHYSICAL_DEVICE_TYPE_DISCRETE_GPU) {
             score += 1000;
@@ -271,23 +307,135 @@ pub fn init(name: [*c]const u8, allocator: std.mem.Allocator) !@This() {
 
         if (preferred) |currentPreferred| {
             if (score > currentPreferred.score) {
-                preferred = .{ .device = device, .score = score };
+                preferred = .{
+                    .device = device,
+                    .features = deviceFeatures,
+                    .queues = queueIndices,
+                    .score = score,
+                };
             }
         } else {
-            preferred = .{ .device = device, .score = score };
+            preferred = .{
+                .device = device,
+                .features = deviceFeatures,
+                .queues = queueIndices,
+                .score = score,
+            };
         }
     }
 
-    if (preferred == null) {
+    if (preferred) {
         std.log.err("no suitable physical device found", .{});
         return error.NoSuitablePhysicalDevice;
     }
 
-    return .{
-        .vulkanInstance = vulkanInstance,
-        .physicalDevice = preferred.?.device,
-        .vulkanDebugMessenger = vulkanDebugMessenger,
+    const physicalDevice = preferred.?.device;
+    const graphicsQueueIndex = preferred.?.graphicsQueueIndex;
+
+    const logicalDeviceExtensions = []const [*c]const u8{
+        c.VK_KHR_SWAPCHAIN_EXTENSION_NAME,
     };
+
+    var logicalDevice: c.VkDevice = undefined;
+    try ensureNoError(c.vkCreateDevice(
+        physicalDevice,
+        &c.VkDeviceCreateInfo{
+            .sType = c.VK_STRUCTURE_TYPE_DEVICE_CREATE_INFO,
+            .pNext = null,
+            .flags = 0,
+            .queueCreateInfoCount = 1,
+            // YOU ARE HERE: you were going to add the creation info for the presentation queue
+            .pQueueCreateInfos = &c.VkDeviceQueueCreateInfo{
+                .sType = c.VK_STRUCTURE_TYPE_DEVICE_QUEUE_CREATE_INFO,
+                .pNext = null,
+                .flags = 0,
+                .queueFamilyIndex = graphicsQueueIndex,
+                .queueCount = 1,
+                .pQueuePriorities = &1.0,
+            },
+            .pEnabledFeatures = std.mem.zeroes(c.VkPhysicalDeviceFeatures),
+            .ppEnabledExtensionNames = logicalDeviceExtensions.ptr,
+            .enabledExtensionCount = logicalDeviceExtensions.len,
+
+            // These are deprecated, and we don't need them
+            .enabledLayerCount = 0,
+            .ppEnabledLayerNames = null,
+        },
+        // TODO: define a proper allocator here using an allocator from the outside
+        null,
+        &logicalDevice,
+    ));
+    std.debug.assert(logicalDevice != null);
+
+    var graphicsQueue: c.VkQueue = undefined;
+    c.vkGetDeviceQueue(logicalDevice, graphicsQueueIndex, 0, &graphicsQueue);
+    std.debug.assert(graphicsQueue != null);
+
+    return .{
+        .physicalDevice = physicalDevice,
+
+        .logicalDevice = logicalDevice,
+        .graphicsQueue = graphicsQueue,
+    };
+}
+
+const QueueIndices = struct {
+    graphics: u32,
+    presentation: u32,
+};
+
+fn getQueueIndices(
+    device: c.VkPhysicalDevice,
+    surface: c.VkSurfaceKHR,
+    allocator: std.mem.Allocator,
+) !?QueueIndices {
+    var queueFamiliesLen: u32 = undefined;
+    c.vkGetPhysicalDeviceQueueFamilyProperties(
+        device,
+        &queueFamiliesLen,
+        null,
+    );
+
+    const queueFamilies = try allocator.alloc(
+        c.VkQueueFamilyProperties,
+        @intCast(queueFamiliesLen),
+    );
+    defer allocator.free(queueFamilies);
+    c.vkGetPhysicalDeviceQueueFamilyProperties(
+        device,
+        &queueFamiliesLen,
+        queueFamilies.ptr,
+    );
+
+    var graphics: ?u32 = null;
+    var presentation: ?u32 = null;
+
+    for (queueFamilies, 0..) |queueFamily, index| {
+        if ((queueFamily.queueFlags & c.VK_QUEUE_GRAPHICS_BIT) != 0) {
+            graphics = index;
+        }
+
+        var supportsPresentation: u32 = c.VK_FALSE;
+        try ensureNoError(c.vkGetPhysicalDeviceSurfaceSupportKHR(
+            device,
+            index,
+            surface,
+            &supportsPresentation,
+        ));
+
+        if (supportsPresentation == c.VK_TRUE) {
+            presentation = index;
+        }
+
+        if (graphics != null and presentation != null) {
+            return .{
+                .graphics = graphics.?,
+                .presentation = presentation.?,
+            };
+        }
+    }
+
+    return null;
 }
 
 pub fn createWaylandSurface(
@@ -316,6 +464,11 @@ pub fn deinit(self: @This()) void {
     DestroyDebugUtilsMessengerEXT(
         self.vulkanInstance,
         self.vulkanDebugMessenger,
+        // TODO: define a proper allocator here using an allocator from the outside
+        null,
+    );
+    c.vkDestroyDevice(
+        self.logicalDevice,
         // TODO: define a proper allocator here using an allocator from the outside
         null,
     );
