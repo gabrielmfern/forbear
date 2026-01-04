7097d6d04f7ed281908a1f4c37b66481f8a7093a
gabriel miranda <gabrielmfern@outlook.com>
2026-01-04 14:08:29 -0300

    show fps in playground

--- playground.zig
+++ playground.zig
@@ -37,6 +37,8 @@ pub fn main() !void {
 
     const inter = try forbear.Font.init("Inter-Regular", @embedFile("Inter-Regular.ttf"));
 
+    var time = std.time.nanoTimestamp();
+    var fps: ?u32 = null;
     while (window.running) {
         defer _ = arenaAllocator.reset(.retain_capacity);
 
@@ -44,15 +46,20 @@ pub fn main() !void {
         const node = forbear.div(.{
             .style = .{
                 .preferredWidth = .grow,
-                .backgroundColor = .{ 0.2, 0.2, 0.2, 1.0 },
+                .background = .{ .color = .{ 0.2, 0.2, 0.2, 1.0 } },
+                .paddingLeft = 10,
+                .paddingRight = 10,
             },
             .children = try forbear.children(.{
+                "fps:",
+                fps,
+                " ",
                 "This is some text introducing things",
                 forbear.div(.{
                     .style = .{
                         .preferredWidth = .{ .fixed = 100 },
                         .preferredHeight = .{ .fixed = 100 },
-                        .backgroundColor = .{ 1.0, 0.0, 0.0, 1.0 },
+                        .background = .{ .color = .{ 1.0, 0.0, 0.0, 1.0 } },
                         .borderRadius = 20,
                     },
                 }),
@@ -60,11 +67,16 @@ pub fn main() !void {
         });
         const layoutBox = try forbear.layout(
             node,
-            .{ .font = inter, .fontSize = 32, .lineHeight = 1.0 },
+            .{ .font = inter, .color = .{ 1.0, 1.0, 1.0, 1.0 }, .fontSize = 32, .lineHeight = 1.0 },
             renderer.viewportSize(),
             .{ @floatFromInt(window.dpi[0]), @floatFromInt(window.dpi[1]) },
             arena,
         );
-        try renderer.drawFrame(&layoutBox);
+        try renderer.drawFrame(&layoutBox, .{ 1.0, 1.0, 1.0, 1.0 });
+
+        const newCurrentTime = std.time.nanoTimestamp();
+        const deltaTime = newCurrentTime - time;
+        time = newCurrentTime;
+        fps = @intFromFloat(@round(@as(f64, @floatFromInt(std.time.ns_per_s)) / @as(f64, @floatFromInt(deltaTime))));
     }
 }
