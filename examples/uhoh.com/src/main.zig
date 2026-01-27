b450027288e2eb2f9da4f1fb43620addb23091a9
gabriel miranda <gabrielmfern@outlook.com>
2026-01-27 07:09:09 -0300

    try a different formatting?

--- examples/uhoh.com/src/main.zig
+++ examples/uhoh.com/src/main.zig
@@ -10,101 +10,88 @@ fn App(props: AppProps) !forbear.Node {
     const arena = try forbear.useArena();
     const isHovering = try forbear.useState(bool, false);
 
-    return forbear.div(.{
-        .style = .{
-            .preferredWidth = .grow,
-            .direction = .topToBottom,
+    return forbear.div(.{ .style = .{
+        .preferredWidth = .grow,
+        .direction = .topToBottom,
+        .horizontalAlignment = .center,
+        .fontWeight = 500,
+        .fontSize = 12,
+    }, .children = try forbear.children(.{
+        forbear.div(.{
+            .style = .{
+                .background = .{ .image = props.comeOnImage },
+                .preferredWidth = .{
+                    .fixed = 165,
+                },
+                .preferredHeight = .{
+                    .fixed = 200,
+                },
+            },
+        }),
+        forbear.div(.{ .style = .{
+            .font = props.spaceGrotesk,
+            .fontWeight = 700,
+            .fontSize = 30,
+            .marginBlock = .{ 10, 10 },
+        }, .children = try forbear.children(.{
+            "Dude, you’re at the bottom of our landing page.",
+        }, arena) }),
+        "Just get the free trial already if you’re that interested.",
+        "You scrolled all the way here.",
+        forbear.div(.{ .style = .{
+            .borderRadius = 6,
+            .borderInlineWidth = @splat(1.5),
+            .borderBlockWidth = @splat(1.5),
+            .marginBlock = .{ 20.0, 0.0 },
+            .background = .{ .color = .{ 0.99, 0.98, 0.96, 1.0 } },
+            .borderColor = .{ 0.0, 0.0, 0.0, 1.0 },
+            .translate = .{
+                0.0,
+                try forbear.useTransition(if (isHovering.*) -4.5 else 0.0, 0.1, forbear.easeInOut),
+            },
+            .shadow = .{
+                .blurRadius = 0.0,
+                .spread = 0.0,
+                .color = .{ 0.0, 0.0, 0.0, 1.0 },
+                .offsetBlock = .{
+                    0.0,
+                    try forbear.useTransition(if (isHovering.*) 4.5 else 0.0, 0.1, forbear.easeInOut),
+                },
+                .offsetInline = @splat(0.0),
+            },
+            .paddingBlock = @splat(20),
+            .paddingInline = @splat(36),
             .horizontalAlignment = .center,
-            .fontWeight = 500,
-            .fontSize = 12,
-        },
-        .children = try forbear.children(.{
-            forbear.div(.{
-                .style = .{
-                    .background = .{ .image = props.comeOnImage },
-                    .preferredWidth = .{
-                        .fixed = 165,
-                    },
-                    .preferredHeight = .{
-                        .fixed = 200,
-                    },
-                },
-            }),
-            forbear.div(.{
-                .style = .{
-                    .font = props.spaceGrotesk,
-                    .fontWeight = 700,
-                    .fontSize = 30,
-                    .marginBlock = .{ 10, 10 },
-                },
-                .children = try forbear.children(.{
-                    "Dude, you’re at the bottom of our landing page.",
-                }, arena),
-            }),
-            "Just get the free trial already if you’re that interested.",
-            "You scrolled all the way here.",
-            forbear.div(.{
-                .style = .{
-                    .borderRadius = 6,
-                    .borderInlineWidth = @splat(1.5),
-                    .borderBlockWidth = @splat(1.5),
-                    .marginBlock = .{ 20.0, 0.0 },
-                    .background = .{ .color = .{ 0.99, 0.98, 0.96, 1.0 } },
-                    .borderColor = .{ 0.0, 0.0, 0.0, 1.0 },
-                    .translate = .{
-                        0.0,
-                        try forbear.useTransition(if (isHovering.*) -4.5 else 0.0, 0.1, forbear.easeInOut),
-                    },
-                    .shadow = .{
-                        .blurRadius = 0.0,
-                        .spread = 0.0,
-                        .color = .{ 0.0, 0.0, 0.0, 1.0 },
-                        .offsetBlock = .{
-                            0.0,
-                            try forbear.useTransition(if (isHovering.*) 4.5 else 0.0, 0.1, forbear.easeInOut),
-                        },
-                        .offsetInline = @splat(0.0),
-                    },
-                    .paddingBlock = @splat(20),
-                    .paddingInline = @splat(36),
-                    .horizontalAlignment = .center,
-                    .verticalAlignment = .center,
-                    .direction = .topToBottom,
-                },
-                .handlers = .{
-                    .onMouseOver = try forbear.eventHandler(
-                        isHovering,
-                        (struct {
-                            fn handler(_: *const forbear.LayoutBox, hovering: *bool) anyerror!void {
-                                hovering.* = true;
-                            }
-                        }).handler,
-                        arena,
-                    ),
-                    .onMouseOut = try forbear.eventHandler(
-                        isHovering,
-                        (struct {
-                            fn handler(_: *const forbear.LayoutBox, hovering: *bool) anyerror!void {
-                                hovering.* = false;
-                            }
-                        }).handler,
-                        arena,
-                    ),
-                },
-                .children = try forbear.children(.{
-                    forbear.div(.{
-                        .style = .{
-                            .fontSize = 18,
-                        },
-                        .children = try forbear.children(.{
-                            "Come on, click on this",
-                        }, arena),
-                    }),
-                    "Don't make me beg",
-                }, arena),
-            }),
-        }, arena),
-    });
+            .verticalAlignment = .center,
+            .direction = .topToBottom,
+        }, .handlers = .{
+            .onMouseOver = try forbear.eventHandler(
+                isHovering,
+                (struct {
+                    fn handler(_: *const forbear.LayoutBox, hovering: *bool) anyerror!void {
+                        hovering.* = true;
+                    }
+                }).handler,
+                arena,
+            ),
+            .onMouseOut = try forbear.eventHandler(
+                isHovering,
+                (struct {
+                    fn handler(_: *const forbear.LayoutBox, hovering: *bool) anyerror!void {
+                        hovering.* = false;
+                    }
+                }).handler,
+                arena,
+            ),
+        }, .children = try forbear.children(.{
+            forbear.div(.{ .style = .{
+                .fontSize = 18,
+            }, .children = try forbear.children(.{
+                "Come on, click on this",
+            }, arena) }),
+            "Don't make me beg",
+        }, arena) }),
+    }, arena) });
 }
 
 fn renderingMain(
