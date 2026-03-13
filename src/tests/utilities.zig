2192d0a2a522d3d681e7d8440868d72707a643f6
Gabriel Miranda <gabrielmfern@outlook.com>
2026-03-13 13:19:05 -0300

    refactor: move tests to a test directory and mirror files (#42)

--- src/tests/utilities.zig
+++ src/tests/utilities.zig
@@ -1,6 +1,17 @@
 const std = @import("std");
-const BaseStyle = @import("node.zig").BaseStyle;
-const forbear = @import("root.zig");
+const BaseStyle = @import("../node.zig").BaseStyle;
+const forbear = @import("../root.zig");
+
+pub const shallowBaseStyle: forbear.BaseStyle = BaseStyle{
+    .font = undefined,
+    .color = .{ 0.0, 0.0, 0.0, 1.0 },
+    .fontSize = 16,
+    .fontWeight = 400,
+    .lineHeight = 1.0,
+    .textWrapping = .none,
+    .blendMode = .normal,
+    .cursor = .default,
+};
 
 pub fn frameMeta(arena: std.mem.Allocator) !forbear.FrameMeta {
     try forbear.registerFont("Inter", @embedFile("./Inter.ttf"));
