eb9e8c88a221b23171e2d665f3f5088f164f043d
Gabriel Miranda <gabrielmfern@outlook.com>
2026-03-25 20:40:58 -0300

    improve some things

--- examples/uhoh.com/src/main.zig
+++ examples/uhoh.com/src/main.zig
@@ -348,30 +348,26 @@ fn App() !void {
                 .maxWidth = 810.0,
                 .alignment = .topCenter,
                 .padding = forbear.Padding.top(22.5).withBottom(30.0),
+                .direction = .topToBottom,
             })({
+                forbear.image(.{
+                    .width = .grow,
+                    .maxWidth = 600,
+                    .blendMode = .multiply,
+                }, try forbear.useImage("uhoh-solution"));
                 forbear.element(.{
-                    .direction = .topToBottom,
+                    .fontWeight = 700,
+                    .fontSize = 22.5,
+                    .margin = forbear.Margin.block(13.5).withBottom(7.5),
+                })({
+                    forbear.text("We're here to reinvent how tech gets done.");
+                });
+                forbear.element(.{
+                    .fontSize = 12.0,
+                    .color = theme.Colors.muted,
                     .alignment = .topCenter,
                 })({
-                    forbear.image(.{
-                        .width = .grow,
-                        .maxWidth = 600,
-                        .blendMode = .multiply,
-                    }, try forbear.useImage("uhoh-solution"));
-                    forbear.element(.{
-                        .fontWeight = 700,
-                        .fontSize = 22.5,
-                        .margin = forbear.Margin.block(13.5).withBottom(7.5),
-                    })({
-                        forbear.text("We're here to reinvent how tech gets done.");
-                    });
-                    forbear.element(.{
-                        .fontSize = 12.0,
-                        .color = theme.Colors.muted,
-                        .alignment = .topCenter,
-                    })({
-                        forbear.text("We're replacing clunky IT with clean, fast, and flexible support. Built for startups and teams that just want things to work.");
-                    });
+                    forbear.text("We're replacing clunky IT with clean, fast, and flexible support. Built for startups and teams that just want things to work.");
                 });
             });
 
