adfd1b7554225f89b5cf87fb2fc91819fe8c8b60
gabriel miranda <gabrielmfern@outlook.com>
2026-04-21 19:23:01 -0300

    style the problem section

--- examples/uhoh.com/src/components/problems.zig
+++ examples/uhoh.com/src/components/problems.zig
@@ -4,66 +4,58 @@ const Button = @import("button.zig").Button;
 
 const Vec4 = @Vector(4, f32);
 
-const issues = [_][]const u8{
-    "You got a cryptic error message on an app. Now you have to submit a ticket.",
-    "Your Google ads literally just got disabled and you're not sure why. Now you have to submit a ticket.",
-    "Someone on your team lost access to a shared account. Now you have to submit a ticket.",
-};
+fn Problem(style: forbear.Style, text: []const u8) !void {
+    forbear.element(style.overwrite(.{
+        .padding = .block(12.0),
+        .fontSize = 14.0,
+        .borderColor = colors.black,
+    }))({
+        forbear.image(.{
+            .width = .{ .fixed = 30.0 },
+            .height = .{ .fixed = 30.0 },
+            .blendMode = .multiply,
+            .margin = .right(20.0),
+        }, try forbear.useImage("uhoh-x-red"));
+        forbear.text(text);
+    });
+}
 
 pub fn Problems() !void {
     forbear.element(.{
         .width = .{ .grow = 1.0 },
         .maxWidth = 940.0,
         .xJustification = .center,
-        .yJustification = .start,
-        .padding = forbear.Padding.top(22.5).withBottom(30.0),
+        .padding = forbear.Padding.block(48.0).withInLine(15.0),
     })({
         forbear.image(.{
-            .width = .{ .grow = 1.0 },
+            .width = .{ .grow = 0.75 },
             .maxWidth = 369,
             .blendMode = .multiply,
-            .margin = forbear.Margin.inLine(0.0).withRight(24.0),
         }, try forbear.useImage("uhoh-problem"));
         forbear.element(.{
             .direction = .vertical,
             .width = .{ .grow = 1.0 },
+            .padding = .left(60.0),
         })({
             forbear.element(.{
-                .fontWeight = 600,
-                .fontSize = 10.5,
+                .fontSize = 24.0,
             })({
                 forbear.text("You're a growing business.");
             });
             forbear.element(.{
                 .fontWeight = 700,
-                .fontSize = 24.0,
-                .margin = forbear.Margin.block(4.5).withBottom(12.0),
+                .fontSize = 40.0,
+                .margin = forbear.Margin.top(20.0).withBottom(25.0),
             })({
                 forbear.text("But your day-to-day has some of this BS in it:");
             });
 
-            for (issues, 0..) |issue, i| {
-                forbear.element(.{
-                    .direction = .horizontal,
-                    .padding = .block(9.0),
-                    .fontSize = 10.5,
-                    .borderWidth = if (i == 0) null else .top(1.5),
-                    .borderColor = colors.black,
-                })({
-                    forbear.image(.{
-                        .width = .{ .fixed = 30.0 },
-                        .height = .{ .fixed = 30.0 },
-                        .blendMode = .multiply,
-                        .margin = .right(7.5),
-                    }, try forbear.useImage("uhoh-x-red"));
-                    forbear.element(.{ .fontSize = 12.0 })({
-                        forbear.text(issue);
-                    });
-                });
-            }
+            try Problem(.{}, "You got a cryptic error message on an app. Now you have to submit a ticket.");
+            try Problem(.{ .borderWidth = .top(2.0) }, "Your Google ads literally just got disabled and you're not sure why. Now you have to submit a ticket.");
+            try Problem(.{ .borderWidth = .top(2.0) }, "Someone on your team lost access to a shared account. Now you have to submit a ticket.");
+
             forbear.element(.{
-                .fontSize = 12.0,
-                .margin = .bottom(30.0),
+                .margin = .bottom(40.0),
             })({
                 forbear.text("Imagine if you could delegate all these issues to a genie?");
             });
