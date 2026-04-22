5c129544ed7debd210d38635771a05fbc0e99653
gabriel miranda <gabrielmfern@outlook.com>
2026-04-22 20:15:00 -0300

    have an outer wrapper for sections

--- examples/uhoh.com/src/components/section.zig
+++ examples/uhoh.com/src/components/section.zig
@@ -2,13 +2,18 @@ const forbear = @import("forbear");
 
 pub fn Section(style: forbear.Style) *const fn (void) void {
     forbear.component("Section")({
-        forbear.element(style.overwrite(.{
+        forbear.element(.{
             .width = .{ .grow = 1.0 },
-            .maxWidth = 940.0,
             .xJustification = .center,
             .padding = forbear.Padding.block(48.0).withInLine(15.0),
-        }))({
-            forbear.componentChildrenSlot();
+        })({
+            forbear.element(style.overwrite(.{
+                .width = .{ .grow = 1.0 },
+                .maxWidth = 940.0,
+                .xJustification = .center,
+            }))({
+                forbear.componentChildrenSlot();
+            });
         });
     });
     return forbear.componentChildrenSlotEnd();
