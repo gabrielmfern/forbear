e7d5c5ada1f320af0172d854a87087d983a9a368
gabriel miranda <gabrielmfern@outlook.com>
2026-04-22 19:35:00 -0300

    jon quote final touches

--- examples/uhoh.com/src/components/jon_quote.zig
+++ examples/uhoh.com/src/components/jon_quote.zig
@@ -5,30 +5,35 @@ const Section = @import("section.zig").Section;
 pub fn JonQuote() !void {
     Section(.{
         .yJustification = .start,
+        .margin = .top(40),
     })({
         forbear.element(.{
+            .width = .{ .grow = 1.0 },
             .direction = .horizontal,
+            .borderRadius = 12.0,
+            .borderWidth = .all(2.0),
+            .borderColor = colors.black,
+            .background = .{ .color = forbear.white },
+            .padding = .all(35.0),
+            .yJustification = .center,
         })({
             forbear.image(.{
-                .width = .{ .fixed = 150.0 },
-                .height = .{ .fixed = 150.0 },
-                .margin = forbear.Margin.inLine(0.0).withRight(12.0),
+                .width = .{ .fixed = 200.0 },
+                .margin = .right(40.0),
             }, try forbear.useImage("uhoh-jon-avatar"));
             forbear.element(.{
                 .direction = .vertical,
+                .width = .{ .grow = 1.0 },
             })({
                 forbear.element(.{
-                    .fontSize = 13.5,
-                    .lineHeight = 1.4,
+                    .fontSize = 32.0,
+                    .lineHeight = 1.0,
+                    .margin = .bottom(20.0),
                 })({
-                    forbear.text("I literally built this because I needed it for myself... it has to be fast, incredibly good and insanely affordable. It's usually impossible to get all three, but we figured it out and we're willing to go to great lengths to let you experience that for yourself.");
+                    forbear.text("“I literally built this because I needed it for myself… it has to be fast, incredibly good and insanely affordable. It's usually impossible to get all three, but we figured it out and we're willing to go to great lengths to let you experience that for yourself.”");
                 });
-                forbear.element(.{
-                    .fontSize = 10.5,
-                    .margin = forbear.Margin.block(9.0).withBottom(0.0),
-                })({
-                    forbear.text("- Jon Sturgeon, CEO of Dingus & Zazzy & Co-Founder of uhoh");
-                });
+                forbear.text("- Jon Sturgeon, CEO of Dingus & Zazzy");
+                forbear.text("& Co-Founder of uhoh");
             });
         });
     });
