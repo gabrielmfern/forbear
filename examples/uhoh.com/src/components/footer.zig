b110d0d32080240cda70ade1bb911d3aab3c518d
gabriel miranda <gabrielmfern@outlook.com>
2026-04-22 20:14:43 -0300

    final touches to footer

--- examples/uhoh.com/src/components/footer.zig
+++ examples/uhoh.com/src/components/footer.zig
@@ -3,38 +3,43 @@ const colors = @import("../colors.zig");
 const Section = @import("section.zig").Section;
 
 pub fn Footer() !void {
-    Section(.{
-        .yJustification = .start,
-    })({
+    Section(.{ .direction = .vertical })({
         forbear.element(.{
+            .width = .{ .grow = 1.0 },
+            .padding = .all(35.0),
+            .borderWidth = .all(2.0),
+            .borderColor = colors.black,
+            .borderRadius = 12.0,
             .direction = .vertical,
         })({
             forbear.element(.{
-                .direction = .horizontal,
-                .xJustification = .center,
                 .yJustification = .center,
+                .width = .{ .grow = 1.0 },
+                .height = .{ .grow = 1.0 },
+                .margin = .bottom(69.0),
             })({
                 forbear.image(.{
                     .width = .{ .fixed = 90.0 },
-                    .margin = forbear.Margin.right(9.0),
                 }, try forbear.useImage("uhoh-logo"));
-                forbear.element(.{ .fontSize = 9.0 })({
+
+                forbear.element(.{ .width = .{ .grow = 1.0 } })({});
+
+                forbear.element(.{ .fontSize = 12.0, .lineHeight = 1.3 })({
                     forbear.text("Privacy Policy");
                 });
             });
             forbear.element(.{
-                .direction = .horizontal,
-                .margin = forbear.Margin.block(12.0).withBottom(0.0),
+                .yJustification = .center,
+                .width = .{ .grow = 1.0 },
+                .height = .{ .grow = 1.0 },
             })({
-                forbear.element(.{
-                    .fontSize = 9.0,
-                    .margin = forbear.Margin.inLine(0.0).withRight(15.0),
-                })({
+                forbear.element(.{ .fontSize = 12.0, .lineHeight = 1.3 })({
                     forbear.text("© 2025 uhoh. All rights reserved.");
                 });
-                forbear.element(.{
-                    .fontSize = 9.0,
-                })({
+
+                forbear.element(.{ .width = .{ .grow = 1.0 } })({});
+
+                forbear.element(.{ .fontSize = 12.0, .lineHeight = 1.3 })({
                     forbear.text("Designed by your lover, Loogart");
                 });
             });
