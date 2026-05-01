c7c9c85e068fd5ad1bb5432852228e72d3c1a323
gabriel miranda <gabrielmfern@outlook.com>
2026-04-30 23:00:46 -0300

    scaffold all chapters
    Signed-off-by: gabriel miranda <gabrielmfern@outlook.com>

---
+++ examples/wayland-book.com/src/components/chapters/surfaces_in_depth.zig
@@ -0,0 +1,29 @@
+const forbear = @import("forbear");
+
+const Heading = @import("../heading.zig").Heading;
+const Paragraph = @import("../paragraph.zig").Paragraph;
+
+pub fn SurfacesInDepth() void {
+    forbear.component(.{
+        .sourceLocation = @src(),
+    })({
+        forbear.element(.{
+            .style = .{
+                .width = .{ .grow = 1.0 },
+                .direction = .vertical,
+                .xJustification = .center,
+                .yJustification = .start,
+                .padding = forbear.Padding.all(15.0),
+                .maxWidth = 750.0,
+            },
+        })({
+            Heading(.{ .level = 1 })({
+                forbear.text("Surfaces in depth");
+            });
+
+            Paragraph()({
+                forbear.text("TODO: write this chapter.");
+            });
+        });
+    });
+}
--- examples/wayland-book.com/src/components/content.zig
+++ examples/wayland-book.com/src/components/content.zig
@@ -3,6 +3,35 @@ const std = @import("std");
 const forbear = @import("forbear");
 
 const Introduction = @import("./chapters/introduction.zig").Introduction;
+const ProtocolDesign = @import("./chapters/protocol_design.zig").ProtocolDesign;
+const WireProtocolBasics = @import("./chapters/wire_protocol_basics.zig").WireProtocolBasics;
+const InterfacesRequestsAndEvents = @import("./chapters/interfaces_requests_and_events.zig").InterfacesRequestsAndEvents;
+const HighLevelProtocolOverview = @import("./chapters/high_level_protocol_overview.zig").HighLevelProtocolOverview;
+const WaylandObjectLifetime = @import("./chapters/wayland_object_lifetime.zig").WaylandObjectLifetime;
+const LibwaylandBasics = @import("./chapters/libwayland_basics.zig").LibwaylandBasics;
+const WaylandProtocolAndLibwayland = @import("./chapters/wayland_protocol_and_libwayland.zig").WaylandProtocolAndLibwayland;
+const DisplaysAndWlDisplay = @import("./chapters/displays_and_wl_display.zig").DisplaysAndWlDisplay;
+const GlobalsAndTheRegistry = @import("./chapters/globals_and_the_registry.zig").GlobalsAndTheRegistry;
+const SurfacesInDepth = @import("./chapters/surfaces_in_depth.zig").SurfacesInDepth;
+const SurfaceBasics = @import("./chapters/surface_basics.zig").SurfaceBasics;
+const SurfaceRegions = @import("./chapters/surface_regions.zig").SurfaceRegions;
+const CompositingAndSubsurfaces = @import("./chapters/compositing_and_subsurfaces.zig").CompositingAndSubsurfaces;
+const BuffersAndSurfaces = @import("./chapters/buffers_and_surfaces.zig").BuffersAndSurfaces;
+const SharedMemoryBuffers = @import("./chapters/shared_memory_buffers.zig").SharedMemoryBuffers;
+const DmaBuf = @import("./chapters/dma_buf.zig").DmaBuf;
+const XdgShellBasics = @import("./chapters/xdg_shell_basics.zig").XdgShellBasics;
+const XdgSurfaces = @import("./chapters/xdg_surfaces.zig").XdgSurfaces;
+const ApplicationWindows = @import("./chapters/application_windows.zig").ApplicationWindows;
+const XdgShellExampleCode = @import("./chapters/xdg_shell_example_code.zig").XdgShellExampleCode;
+const SeatHandlingInput = @import("./chapters/seat_handling_input.zig").SeatHandlingInput;
+const PointerInput = @import("./chapters/pointer_input.zig").PointerInput;
+const KeyboardInput = @import("./chapters/keyboard_input.zig").KeyboardInput;
+const TouchInput = @import("./chapters/touch_input.zig").TouchInput;
+const SeatExampleCode = @import("./chapters/seat_example_code.zig").SeatExampleCode;
+const BeyondTheBasics = @import("./chapters/beyond_the_basics.zig").BeyondTheBasics;
+const XdgShellInDepth = @import("./chapters/xdg_shell_in_depth.zig").XdgShellInDepth;
+const ClipboardAndDnd = @import("./chapters/clipboard_and_dnd.zig").ClipboardAndDnd;
+const HighDpiSupport = @import("./chapters/high_dpi_support.zig").HighDpiSupport;
 const Heading = @import("heading.zig").Heading;
 
 fn Topbar() void {
@@ -48,6 +77,35 @@ pub fn Content(activeChatper: *usize) void {
 
             switch (activeChatper.*) {
                 0 => Introduction(),
+                1 => ProtocolDesign(),
+                2 => WireProtocolBasics(),
+                3 => InterfacesRequestsAndEvents(),
+                4 => HighLevelProtocolOverview(),
+                5 => WaylandObjectLifetime(),
+                6 => LibwaylandBasics(),
+                7 => WaylandProtocolAndLibwayland(),
+                8 => DisplaysAndWlDisplay(),
+                9 => GlobalsAndTheRegistry(),
+                10 => SurfacesInDepth(),
+                11 => SurfaceBasics(),
+                12 => SurfaceRegions(),
+                13 => CompositingAndSubsurfaces(),
+                14 => BuffersAndSurfaces(),
+                15 => SharedMemoryBuffers(),
+                16 => DmaBuf(),
+                17 => XdgShellBasics(),
+                18 => XdgSurfaces(),
+                19 => ApplicationWindows(),
+                20 => XdgShellExampleCode(),
+                21 => SeatHandlingInput(),
+                22 => PointerInput(),
+                23 => KeyboardInput(),
+                24 => TouchInput(),
+                25 => SeatExampleCode(),
+                26 => BeyondTheBasics(),
+                27 => XdgShellInDepth(),
+                28 => ClipboardAndDnd(),
+                29 => HighDpiSupport(),
                 else => {
                     forbear.element(.{
                         .style = .{
