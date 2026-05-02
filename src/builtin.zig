214db26294c0171fda10c2fa8c4440ad24ed7853
gabriel miranda <gabrielmfern@outlook.com>
2026-05-02 18:30:51 -0300

    add scroll bar component to builtins
    Signed-off-by: gabriel miranda <gabrielmfern@outlook.com>

--- src/builtin.zig
+++ src/builtin.zig
@@ -3,6 +3,7 @@ const builtin = @import("builtin");
 const forbear = @import("root.zig");
 
 const Vec2 = @Vector(2, f32);
+const Vec4 = @Vector(4, f32);
 
 pub fn FpsCounter() void {
     forbear.component(.{
@@ -100,5 +101,64 @@ pub fn useScrolling() Vec2 {
         );
         node.childrenOffset = -aniamtedOffset;
         return aniamtedOffset;
+    }
+}
+
+pub fn ScrollBar(scrollingOffset: Vec2) void {
+    if (forbear.useNodeMeasurement()) |parentMeasurement| {
+        if (parentMeasurement.contentSize[1] > parentMeasurement.size[1]) {
+            forbear.component(.{ .sourceLocation = @src() })({
+                const expanded = forbear.useState(bool, false);
+
+                const scrollbarWidth = forbear.useTransition(
+                    f32,
+                    if (expanded.*) 11.0 else 7.0,
+                    0.15,
+                    forbear.easeOut,
+                );
+
+                // track
+                forbear.element(.{
+                    .style = .{
+                        .placement = .{ .relative = .{ parentMeasurement.size[0] - scrollbarWidth, 0.0 } },
+                        .width = .{ .fixed = scrollbarWidth },
+                        .height = .{ .fixed = parentMeasurement.size[1] },
+                    },
+                })({
+                    if (forbear.on(.mouseEnter)) {
+                        expanded.* = true;
+                    }
+                    if (forbear.on(.mouseLeave)) {
+                        expanded.* = false;
+                    }
+
+                    // thumb
+                    forbear.element(.{
+                        .style = .{
+                            .width = .{ .grow = 1.0 },
+                            .height = .{ .fixed = parentMeasurement.size[1] * parentMeasurement.size[1] / parentMeasurement.contentSize[1] },
+                            .placement = .{
+                                .relative = Vec2{
+                                    0,
+                                    if (scrollingOffset[1] == 0)
+                                        0.0
+                                    else
+                                        parentMeasurement.size[1] * (scrollingOffset[1] / parentMeasurement.contentSize[1]),
+                                },
+                            },
+                            .borderRadius = 6.0,
+                            .background = .{
+                                .color = forbear.useTransition(
+                                    Vec4,
+                                    if (expanded.*) forbear.hex("#D0D0D0") else forbear.hex("#8D8D8D"),
+                                    0.15,
+                                    forbear.easeOut,
+                                ),
+                            },
+                        },
+                    })({});
+                });
+            });
+        }
     }
 }
